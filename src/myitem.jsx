c84e587e303294788c92842c0cde9d4f19d38a42
Claudio Salas <djsalas2133@gmail.com>
2023-06-21 14:37:19 -0700

    Add untracked files

---
+++ src/myitem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Item extends React.Component {
+
+constructor(props){
+  super(props)
+
+}
+
+  render(){
+    return (
+
+  <div>
+      <h1> Pictures go here  </h1>
+  </div>
+
+      )
+  }
+}
+
+
+export default Item;
