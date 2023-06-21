5812acdf3a7fa691bd828e1934878f18b8d1bc7b
Claudio Salas <djsalas2133@gmail.com>
2023-06-21 14:29:54 -0700

    Update modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
+import leia_bear from '/src/leia_bear.png';
+import Item from '/src/myitem';
+import About from '/src/about';
+import Family from '/src/family';
+import AskMe from '/src/askMe';
+import Navbar from '/src/navbar';
+
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />
+      < Home />
+
+      <Routes>
+        {/* <Route path="/" element={< Home />} /> */}
+        <Route path="/myitem" element={< Item />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+function Home() {
+  return (
+    <>
+      <div className="aboutleiaContainer">
+        <About />
+        <Family />
+      </div>
+
+      <div className="mainImage">
+        <span href="" target="">
+          <img src={leia_bear} className="logo" alt="Leia Bear Logo" />
+        </span>
+      </div>
+
+      <div className="leiaIntro">
+        <h1> Hi Im Leia Bear </h1>
+      </div>
+
+      <div className="aboutleiaContainer">
+        <AskMe />
+        <Link to="/myitem">
+          <button>
+            <h1> Pictures </h1>
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default App;
